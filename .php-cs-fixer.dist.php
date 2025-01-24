bd882b178f0f8349e819f05f98b6bdd023da40a3
nikophil <10139766+nikophil@users.noreply.github.com>
2025-01-24 22:57:09 +0000

    bot: sync with template [skip ci]

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,13 +7,6 @@ $file = __DIR__.'/.php-cs-fixer.temp.php';
     \file_get_contents('https://raw.githubusercontent.com/zenstruck/.github/main/.php-cs-fixer.dist.php')
 );
 
-$finder = PhpCsFixer\Finder::create()
-    ->in([__DIR__.'/src', __DIR__.'/tests'])
-
-    // we want to keep the traits in "wrong order"
-    ->notName('KernelTestCaseWithBothTraitsInWrongOrderTest.php')
-;
-
 try {
     return require $file;
 } finally {
